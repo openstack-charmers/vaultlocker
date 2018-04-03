a86254a08ba28cd79efc4ce64154ea8caaae49ac
James Page <james.page@ubuntu.com>
2018-04-03 14:45:14 +0100

    Drop file support

--- vaultlocker/shell.py
+++ vaultlocker/shell.py
@@ -17,7 +17,6 @@ import argparse
 import hvac
 import logging
 import os
-import shutil
 import socket
 import tenacity
 import uuid
@@ -45,64 +44,10 @@ def _get_vault_path(device_uuid, config):
                              device_uuid)
 
 
-def _store_file_in_vault(args, client, config):
-    source = args.source[0]
-    if not os.path.exists(source):
-        raise ValueError('Unable to locate source file {}'.format(source))
-
-    source_uuid = str(uuid.uuid4())
-    logger.info('Storing secret {} in vault'.format(source_uuid))
-
-    vault_path = _get_vault_path(source_uuid, config)
-
-    with open(source, 'rb') as input_file:
-        input_data = input_file.read()
-        client.write(vault_path,
-                     content=input_data,
-                     source_path=source)
-        stored_data = \
-            client.read(vault_path)
-        assert input_data == stored_data['data']['content']
-        assert source == stored_data['data']['source_path']
-
-    if not os.path.exists(RUN_VAULTLOCKER):
-        os.makedirs(RUN_VAULTLOCKER)
-
-    new_path = os.path.join(RUN_VAULTLOCKER, source_uuid)
-    shutil.move(source, new_path)
-    os.symlink(new_path, source)
-    print(source_uuid)
-
-
-def _retrieve_file_from_vault(args, client, config):
-    target_uuid = args[0]
-    new_path = os.path.join(RUN_VAULTLOCKER, target_uuid)
-    if os.path.exists(new_path):
-        logger.info('Secret {} already on disk, skipping'.format(target_uuid))
-        return
-
-    vault_path = _get_vault_path(target_uuid, config)
-
-    logger.info('Retrieving secret {} from vault'.format(target_uuid))
-    stored_file = client.read(vault_path)
-
-    if not os.path.exists(RUN_VAULTLOCKER):
-        os.makedirs(RUN_VAULTLOCKER)
-
-    with open(new_path, 'wb') as target:
-        os.fchmod(target.fileno(), 0o400)
-        target.write(stored_file['data']['content'])
-
-    original_source = stored_file['data']['source_path']
-    if os.path.exists(original_source):
-        os.remove(original_source)
-    os.symlink(new_path, original_source)
-
-
 def _encrypt_block_device(args, client, config):
     block_device = args.block_device[0]
     key = dmcrypt.generate_key()
-    block_uuid = str(uuid.uuid4())
+    block_uuid = str(uuid.uuid4()) if not args.uuid else args.uuid
     vault_path = _get_vault_path(block_uuid, config)
 
     dmcrypt.luks_format(key, block_device, block_uuid)
@@ -146,14 +91,6 @@ def _do_it_with_persistence(func, args, config):
     _do_it()
 
 
-def store(args, config):
-    _do_it_with_persistence(_store_file_in_vault, args, config)
-
-
-def retrieve(args, config):
-    _do_it_with_persistence(_retrieve_file_from_vault, args, config)
-
-
 def encrypt(args, config):
     _do_it_with_persistence(_encrypt_block_device, args, config)
 
@@ -184,26 +121,13 @@ def main():
         help="Time in seconds to continue retrying to connect to Vault"
     )
 
-    store_parser = subparsers.add_parser(
-        'store',
-        help='Store new file in Vault'
-    )
-    store_parser.add_argument('source',
-                              metavar='SOURCE', nargs=1)
-    store_parser.set_defaults(func=store)
-
-    retrieve_parser = subparsers.add_parser(
-        'retrieve',
-        help='Retrieve file by UUID from Vault'
-    )
-    retrieve_parser.add_argument('target_uuid',
-                                 metavar='TARGET_UUID', nargs=1)
-    retrieve_parser.set_defaults(func=retrieve)
-
     encrypt_parser = subparsers.add_parser(
         'encrypt',
         help='Encrypt a block device and store its key in Vault'
     )
+    encrypt_parser.add_argument('--uuid',
+                                dest="uuid",
+                                help="UUID to use to reference encryption key")
     encrypt_parser.add_argument('block_device',
                                 metavar='BLOCK_DEVICE', nargs=1,
                                 help="Full path to block device to encrypt")
