1de725af2f5fac0888ed713c9c4d49e7e48b5603
James Page <james.page@ubuntu.com>
2018-04-12 10:08:21 +0100

    Add basic functional tests for interaction with vault

---
+++ vaultlocker/tests/functional/base.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2010-2011 OpenStack Foundation
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import hvac
+import mock
+import os
+import uuid
+
+from oslotest import base
+
+
+TEST_POLICY = '''
+path "{backend}/*" {{
+  capabilities = ["create", "read", "update", "delete", "list"]
+}}
+'''
+
+
+class VaultlockerFuncBaseTestCase(base.BaseTestCase):
+
+    """Test case base class for all functional tests."""
+
+    def setUp(self):
+        super(VaultlockerFuncBaseTestCase, self).setUp()
+        self.vault_client = None
+
+        self.vault_addr = os.environ.get('PIFPAF_VAULT_ADDR')
+        self.root_token = os.environ.get('PIFPAF_ROOT_TOKEN')
+
+        self.test_uuid = str(uuid.uuid4())
+        self.vault_backend = 'vaultlocker-test-{}'.format(self.test_uuid)
+        self.vault_policy = 'vaultlocker-policy-{}'.format(self.test_uuid)
+        self.vault_approle = 'vaultlocker-approle-{}'.format(self.test_uuid)
+
+        if not self.vault_addr or not self.root_token:
+            return
+
+        self.vault_client = hvac.Client(url=self.vault_addr,
+                                        token=self.root_token)
+
+        self.vault_client.enable_secret_backend(
+            backend_type='kv',
+            description='vault test backend',
+            mount_point=self.vault_backend
+        )
+
+        try:
+            self.vault_client.enable_auth_backend('approle')
+        except hvac.exceptions.InvalidRequest:
+            pass
+
+        self.vault_client.set_policy(
+            name=self.vault_policy,
+            rules=TEST_POLICY.format(backend=self.vault_backend)
+        )
+
+        self.vault_client.create_role(
+            self.vault_approle,
+            token_ttl='60s',
+            token_max_ttl='60s',
+            policies=[self.vault_policy],
+            bind_secret_id='false',
+            bound_cidr_list='127.0.0.1/32')
+        self.approle_uuid = self.vault_client.get_role_id(self.vault_approle)
+
+        self.test_config = {
+            'url': self.vault_addr,
+            'approle': self.approle_uuid,
+            'backend': self.vault_backend,
+        }
+        self.config = mock.MagicMock()
+        self.config.get.side_effect = lambda _, k: self.test_config.get(k)
+
+    def tearDown(self):
+        super(VaultlockerFuncBaseTestCase, self).tearDown()
+        if self.vault_client:
+            self.vault_client.disable_secret_backend(self.vault_backend)
+            self.vault_client.delete_policy(self.vault_policy)
---
+++ vaultlocker/tests/functional/test_keystorage.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2010-2011 OpenStack Foundation
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+
+from vaultlocker import shell
+from vaultlocker.tests.functional import base
+
+
+@mock.patch.object(shell, 'systemd')
+@mock.patch.object(shell.dmcrypt, 'luks_format')
+@mock.patch.object(shell.dmcrypt, 'luks_open')
+class KeyStorageTestCase(base.VaultlockerFuncBaseTestCase):
+
+    """Test storage and retrieval of dm-crypt keys from vault"""
+
+    def test_encrypt(self, _luks_open, _luks_format, _systemd):
+        """Test encrypt function stores correct data in vault"""
+        args = mock.MagicMock()
+        args.uuid = 'passed-UUID'
+        args.block_device = ['/dev/sdb']
+        args.retry = -1
+
+        shell.encrypt(args, self.config)
+        _luks_format.assert_called_once_with(mock.ANY,
+                                             '/dev/sdb',
+                                             'passed-UUID')
+        _luks_open.assert_called_once_with(mock.ANY,
+                                           'passed-UUID')
+        _systemd.enable.assert_called_once_with(
+            'vaultlocker-decrypt@passed-UUID.service'
+        )
+
+        stored_data = self.vault_client.read(
+            shell._get_vault_path('passed-UUID',
+                                  self.config)
+        )
+        self.assertIsNotNone(stored_data,
+                             'Key data missing from vault')
+        self.assertTrue('dmcrypt_key' in stored_data['data'],
+                        'dm-crypt key data is missing')
+
+    def test_decrypt(self, _luks_open, _luks_format, _systemd):
+        """Test decrypt function retrieves correct key from vault"""
+        args = mock.MagicMock()
+        args.uuid = ['passed-UUID']
+        args.retry = -1
+
+        self.vault_client.write(shell._get_vault_path('passed-UUID',
+                                                      self.config),
+                                dmcrypt_key='testkey')
+
+        shell.decrypt(args, self.config)
+        _luks_format.assert_not_called()
+        _systemd.enable.assert_not_called()
+        _luks_open.assert_called_once_with('testkey',
+                                           'passed-UUID')
